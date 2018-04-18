5659577ac1233c38e68f4d25535e55422b11e540
zzp <2269225970@qq.com>
2018-04-18 18:47:02 +0800

    complete

--- src/main/java/practice1/Security.java
+++ src/main/java/practice1/Security.java
@@ -12,27 +12,14 @@ public class Security {
 
     public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
 
-        boolean isAccess = false;
-        if (user == null) {
-            return isAccess;
+        if (user == null || permission == null || permissions.size() == 0) {
+            return false;
         }
 
-        if (permission == null) {
-            return isAccess;
+        if (securityChecker.isAdmin() || this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
+            return true;
         }
 
-        if (permissions.size() == 0) {
-            return isAccess;
-        }
-
-        if (securityChecker.isAdmin()) {
-            isAccess = true;
-        }
-
-        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
-            isAccess = true;
-        }
-
-        return isAccess;
+        return false;
     }
 }
